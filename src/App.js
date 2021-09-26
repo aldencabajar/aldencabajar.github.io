64a5ed37eb1754edddd045768fa54fef6321e8c4
aldenacabajar <aldenacabajar@gmail.com>
2021-09-26 20:20:54 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 import './App.css';
 import Navbar from './components/Navbar'
-import About from './components/About';
 import Home from './components/Home';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact';
 import Resume from './components/Resume/Resume'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import {slide as Menu} from 'react-burger-menu'
 import { useEffect } from 'react';
+import { useState } from 'react';
+
 
 const App=()=>{
   const stylePageWrap = {
     height:"100%",
   }
+  const [menuStateOpen, menuSetState] = useState(false)
+
+  const handleMenuClick=(e)=>{menuSetState(false)}
+  const handleMenuStateOnChange=(state)=>{
+    menuSetState(state.isOpen)
+  }
+
+
   useEffect(()=>{
     document.title = "Alden Cabajar"
-
-  },[])
+    console.log(menuStateOpen)
+  },[menuStateOpen])
   const styleOther ={height:"100%"}
   return (
     <Router>
       <div className="App" id="outer-container" style={styleOther}>
-        <Menu width={500} right={true} >
-            <a id="about"  href="/about">about</a>
-            <a id="contact" href="/contact">project</a>
-            <a id="project"  href="/projects">resume</a>
+        <Menu width={500} right={true} isOpen={menuStateOpen}
+        onStateChange={handleMenuStateOnChange}>
+            <Link to='/' onClick={handleMenuClick}>home</Link>
+            <Link to='/projects' onClick={handleMenuClick}>projects</Link>
+            <Link to='/resume' onClick={handleMenuClick}>resume</Link>
         </Menu>
         <div id="page-wrap" style={stylePageWrap}>
             <Navbar/>
             <Switch>
               <Route path = "/" exact component ={Home}/>
-              <Route path = "/about" component={About}/>
               <Route path = "/projects" component={Projects}/>
               <Route path = "/contact" component={Contact}/>
               <Route path = "/resume" component={Resume}/>
