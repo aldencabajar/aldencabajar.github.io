98f9553c5a4d2ee84b65617122b667f2cefde7de
aldenacabajar <aldenacabajar@gmail.com>
2021-10-04 02:30:00 +0800

    updates and delete file

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Layout from 'components/layout'
+import { graphql } from 'gatsby';
 
-export default function Blog() {
+export default function Blog({data}) {
+    const {posts} = data.blog
     return(
         <Layout>
             <div className='section-body'>
@@ -10,8 +12,13 @@ export default function Blog() {
                     <h3>Documented ramblings and learnings.</h3>
                 </header>
             </div>
-
         </Layout>
     )
 
 }
+ export const mdQuery = graphql` 
+ query MyQuery {
+     blog: allMar
+ }
+
+
